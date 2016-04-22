afd5075bfb451c8f7688a824767b008cc85cf129
Evan Pavlica <evan@xicreative.net>
2016-04-21 20:50:14 -0700

    Save files to DB

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,25 +1,33 @@
 (function(document) {
-  'use strict';
+    'use strict';
 
-  var app = document.querySelector('#app');
+    window.app = document.querySelector('#app');
+    var db;
 
-  // Sets app default base URL
-  app.baseUrl = '/';
-  if (window.location.port === '') {  // if production
-    // Uncomment app.baseURL below and
-    // set app.baseURL to '/your-pathname/' if running from folder in production
-    // app.baseUrl = '/polymer-starter-kit/';
-  }
-
-  // Listen for template bound event to know when bindings
-  // have resolved and content has been stamped to the page
-  app.addEventListener('dom-change', function() {
-    // console.log('Our app is ready to rock!');
-  });
-
-  window.addEventListener('WebComponentsReady', function() {
-    // imports are loaded and elements have been registered
-  });
+    // Sets app default base URL
+    app.baseUrl = '/';
+    if (window.location.port === '') {  // if production
+        // Uncomment app.baseURL below and
+        // set app.baseURL to '/your-pathname/' if running from folder in production
+        // app.baseUrl = '/polymer-starter-kit/';
+    }
 
 
+    // Listen for template bound event to know when bindings
+    // have resolved and content has been stamped to the page
+    app.addEventListener('dom-change', function() {
+        // console.log('Our app is ready to rock!');
+    });
+
+    window.addEventListener('WebComponentsReady', function() {
+        // imports are loaded and elements have been registered
+        // Init the Database
+        db = new TessituraDatabase();
+    });
+
+    // Database loaded.
+    window.addEventListener('dbAttached', function() {
+        app.db = db;
+        app.$.library.listTitles();
+    });
 })(document);
--- app/scripts/tessitura-database.js
+++ app/scripts/tessitura-database.js
@@ -14,7 +14,12 @@ class TessituraDatabase {
 
         // Successfully Opened IndexedDB
         request.onsuccess = (e) => {
-            _this.db = e.target.result;
+            _this._db = e.target.result;
+            let event = new Event('dbAttached', {
+                bubbles: true,
+                cancelable: false
+            });
+            document.dispatchEvent(event);
         };
 
         // Failed to Open IndexedDB
@@ -25,10 +30,140 @@ class TessituraDatabase {
         // Set Up the DB
         request.onupgradeneeded = (e) => {
             let db = e.target.result;
-            _this.setupDB(db).then(() => {
-                _this.db = db;
+            _this._setupDB(db).then(() => {
+                _this._db = db;
             });
         };
+    }
+
+    /**
+     * Get all Tessitura titles from IndexedDB
+     * @returns {Promise} resolves with Array of titles
+     */
+    allTitles() {
+        return new Promise((resolve, reject) => {
+            let files = [];
+            let cursor = this._objectStore(this.TITLE_STORE_NAME).openCursor();
+
+            cursor.onsuccess = (e) => {
+                let file = e.target.result;
+                if (file) {
+                    files.push(file.value);
+                    file.continue();
+                } else {
+                    resolve(files);
+                }
+            };
+
+            cursor.onerror = (e) => {
+                console.warn(`[TessituraDatabase] Failed to get titles: ${e.target.errorCode}`);
+                reject(e.target.errorCode);
+            };
+        });
+    }
+
+    /**
+     * Delete a record from IndexedDB
+     * @param {String} type - type of record
+     * @param {String} key - key for record
+     * @returns {Promise} - resolves with record
+     */
+    delete(type, key) {
+        let _this = this;
+        let store = this[`${type.toUpperCase()}_STORE_NAME`];
+        return new Promise(function(resolve, reject) {
+            let request = _this._objectStore(store, 'readonly').delete(key);
+            request.onsuccess = (e) => {
+                console.log(`[TessituraDatabase] Deleted ${type} ${key}`);
+                resolve(request.result);
+            };
+
+            request.onerror = (e) => {
+                console.warn(`[TessituraDatabase] Failed to delete ${type} ${key}`);
+                reject(new Error(e.target.errorCode));
+            };
+        });
+    }
+
+    /**
+     * Get specific record from IndexedDB
+     * @param {String} type - type of record
+     * @param {String} key - key for record
+     * @returns {Promise} - resolves with record
+     */
+    retrieve(type, key) {
+        let _this = this;
+        let store = this[`${type.toUpperCase()}_STORE_NAME`];
+        return new Promise(function(resolve, reject) {
+            let request = _this._objectStore(store, 'readonly').get(key);
+            request.onsuccess = (e) => {
+                console.log(`[TessituraDatabase] Updated ${type} ${key}`);
+                resolve(request.result);
+            };
+
+            request.onerror = (e) => {
+                console.warn(`[TessituraDatabase] Failed to update ${type} ${key}`);
+                reject(new Error(e.target.errorCode));
+            };
+        });
+    }
+
+    /**
+     * Save a record to IndexedDB
+     * @param {String} type - type of record
+     * @param {Object} record - Object containing record data
+     * @returns {Promise} resolves with new record key
+     */
+    save(type, record) {
+        let _this = this;
+        let store = this[`${type.toUpperCase()}_STORE_NAME`];
+
+        return new Promise(function(resolve, reject) {
+            let request = _this._objectStore(store, 'readwrite').add(record);
+            request.onsuccess = (e) => {
+                console.log(`[TessituraDatabase] Saved ${type}`);
+                resolve(e.target.result);
+            };
+
+            request.onerror = (e) => {
+                console.warn(`[TessituraDatabase] Failed to save ${type}: ${e}`);
+                reject(new Error(e.target.errorCode));
+            };
+        });
+    }
+
+    /**
+     * Update a record to IndexedDB
+     * @param {String} type - type of record
+     * @param {String} key - key of record to be updated
+     * @param {Object} record - Object containing record data
+     * @returns {Promise} resolves with updated record's key
+     */
+    update(type, key, record) {
+        let _this = this;
+        let store = this[`${type.toUpperCase()}_STORE_NAME`];
+
+        return new Promise(function(resolve, reject) {
+            let request = _this._objectStore(store, 'readwrite').put(record, key);
+            request.onsuccess = (e) => {
+                console.log(`[TessituraDatabase] Saved ${type}`);
+                resolve(e.target.result);
+            };
+
+            request.onerror = (e) => {
+                console.warn(`[TessituraDatabase] Failed to save ${type}: ${e}`);
+                reject(new Error(e.target.errorCode));
+            };
+        });
+    }
+
+    /**
+     * @param {String} storeName - name of IndexedDB ObjectStore
+     * @param {String} mode - IndexedDB access mode. Valid: 'readonly' (default), 'readwrite', or 'versionchange'
+     * @returns {IDBObjectStore} - object store
+     */
+    _objectStore(storeName, mode = 'readonly') {
+        return this._db.transaction(storeName, mode).objectStore(storeName);
     }
 
     /**
@@ -42,7 +177,7 @@ class TessituraDatabase {
      * @param {IDBDatabase} db
      * @returns {Promise}
      */
-    setupDB(db) {
+    _setupDB(db) {
         let completed = {
             files: false,
             titles: false,
@@ -55,8 +190,8 @@ class TessituraDatabase {
 
         return new Promise((resolve, reject) => {
             let handleEvents = function(objectStore, type) {
-                console.log(objectStore);
                 objectStore.onsuccess = () => {
+                    console.info(`[TessituraDatabase] Initialized ${type} objectStore`);
                     completed[type] = true;
                     if (done()) { resolve(); }
                 };
@@ -103,87 +238,5 @@ class TessituraDatabase {
             let prefStore = db.createObjectStore( this.PREFERENCE_STORE_NAME, { keyPath: 'pref' });
             handleEvents(prefStore, 'prefs');
         });
-    }
-
-    /**
-     * Get all Tessitura titles from IndexedDB
-     * @returns {Promise} resolves with Array of titles
-     */
-    allTitles() {
-        return new Promise((resolve, reject) => {
-            let files = [];
-            let cursor = this._objectStore(this.TITLE_STORE_NAME).openCursor();
-
-            cursor.onsuccess = (e) => {
-                let file = e.target.result;
-                if (file) {
-                    files.push(file.value);
-                    file.continue();
-                } else {
-                    resolve(files);
-                }
-            };
-
-            cursor.onerror = (e) => {
-                console.warn(`[TessituraDatabase] Failed to get titles: ${e.target.errorCode}`);
-                reject(e.target.errorCode);
-            };
-        });
-    }
-
-    /**
-     * Get specific title from IndexedDB
-     * @param {String} type - type of record
-     * @param {Number} id - key for title
-     * @returns {Promise} - resolves with title record
-     */
-    retrieve(type, id) {
-        let _this = this;
-        let store = this[`${type.toUpperCase()}_STORE_NAME`];
-        return new Promise(function(resolve, reject) {
-            let request = _this._objectStore(store, 'readonly').get(id);
-            request.onsuccess = (e) => {
-                console.log(`[TessituraDatabase] Retrieved ${type} ${id}`);
-                resolve(request.result);
-            };
-
-            request.onerror = (e) => {
-                console.warn(`[TessituraDatabase] Failed to retrieve ${type} ${id}`);
-                reject(new Error(e.target.errorCode));
-            };
-        });
-    }
-
-    /**
-     * Save a record to IndexedDB
-     * @param {String} type - type of record
-     * @param {Object} record - Object containing record data
-     * @returns {Promise} resolves with new item ID.
-     */
-    save(type, record) {
-        let _this = this;
-        let store = this[`${type.toUpperCase()}_STORE_NAME`];
-
-        return new Promise(function(resolve, reject) {
-            let request = _this._objectStore(store, 'readwrite').add(record);
-            request.onsuccess = (e) => {
-                console.log(`[TessituraDatabase] Saved ${type}`);
-                resolve(e.target.result);
-            };
-
-            request.onerror = (e) => {
-                console.warn(`[TessituraDatabase] Failed to save ${type}: ${e}`);
-                reject(new Error(e.target.errorCode));
-            };
-        });
-    }
-
-    /**
-     * @param {String} storeName - name of IndexedDB ObjectStore
-     * @param {String} mode - IndexedDB access mode. Valid: 'readonly' (default), 'readwrite', or 'versionchange'
-     * @returns {IDBObjectStore} - object store
-     */
-    _objectStore(storeName, mode = 'readonly') {
-        return this.db.transaction(storeName, mode).objectStore(storeName);
     }
 }
